9580cfc47aa9e463033e4d56bfb95fe59f4dd445
Hajime Nutahara <hajime.nutahara@gmail.com>
2019-12-17 00:20:30 +0900

    add Session controller and login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+
+  def curret_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
