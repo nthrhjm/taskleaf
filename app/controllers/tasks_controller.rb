bd5fc2ae20919ade6ed40d47069d2e82062abd2b
Hajime Nutahara <hajime.nutahara@gmail.com>
2019-12-17 10:35:57 +0900

    fix Task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,7 +31,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "たすく「#{@task.name}」を削除しました。"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
